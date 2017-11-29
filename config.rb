7b4092836c244c0bd04b1eb7537c1337fa897517
sarah <srhbutts@gmail.com>
2017-11-29 02:28:51 +0000

    change default options to exclude proper nouns

--- config.rb
+++ config.rb
@@ -23,7 +23,7 @@
 OPTIONS = {
     :has_dictionary_def => true,
     :include_part_of_speech => "",
-    :exclude_part_of_speech => "",
+    :exclude_part_of_speech => "proper-noun",
     :min_corpus_count => 5000,
     :max_corpus_count => -1,
     :min_dictionary_count => 1,
