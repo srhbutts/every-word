7653e164aea38e273227d8ba002c6f8a97590852
sarah <srhbutts@gmail.com>
2017-11-30 21:38:20 +0000

    revert dotenv addition, not meant for deployment

--- config.rb
+++ config.rb
@@ -1,25 +1,3 @@
-# Most config is handled via environment variables. Make sure to set the following.
-
-# Set this to pick the word that corresponds to your bot.
-    # ENV["EVERY_WORD_WORD"]
-
-# TWITTER ENV VARIABLES THAT MUST BE SET
-    # ENV["CONSUMER_KEY"]
-    # ENV["CONSUMER_SECRET"]
-    # ENV["ACCESS_TOKEN"]
-    # ENV["ACCESS_TOKEN_SECRET"]
-    
-# WORDNIK ENV VARIABLES THAT MUST BE SET
-    # ENV['WORDNIK_KEY']
-    
-# Wordnik Query Options
-#
-# Lower numbers with the corpus count range correspond to words that are used 
-# less often, and vice versa.
-#
-# See the corresponding wordnik API page for more details on other params:
-# http://developer.wordnik.com/docs.html#!/words/getRandomWord_get_4
-
 OPTIONS = {
     :has_dictionary_def => true,
     :include_part_of_speech => "",
